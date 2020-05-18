b07fb3a69d09794d534eca2eff80ab6ed6123b9b
Roger <roger.8b@gmail.com>
2020-05-18 00:51:03 -0300

    First commit

---
+++ app/src/main/java/com/rms/gitapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rms.gitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
