89b042fd0fc6e04a3a6ba0459eec63b6a3915b72
Roger <roger.8b@gmail.com>
2020-05-31 16:45:54 -0300

    Danger Install Detekt

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -6,4 +6,4 @@ echo "##### Install Ktlint"
 # brew install ktlint
 
 echo "##### Execute Danger"
-bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
+bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=false
