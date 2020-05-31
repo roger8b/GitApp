0bae49f09815727fd4d604ddc77524ef7ca820b1
Roger <roger.8b@gmail.com>
2020-05-31 00:56:18 -0300

    Danger Install Ktlint

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+echo "##### Install Ktlint"
+curl --output /usr/local/bin/ktlint -sL https://github.com/pinterest/ktlint/releases/download/$KTLINT_VERSION/ktlint && chmod a+x /usr/local/bin/ktlint
+
 echo "##### Execute Danger"
 bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
