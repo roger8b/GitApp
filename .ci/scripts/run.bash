91610da3be7b36bd0e8f66cc6785337044b48747
Roger <roger.8b@gmail.com>
2020-05-30 22:33:17 -0300

    Test actions running script

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -2,17 +2,16 @@
 
 set -euo pipefail
 
-echo "Configure Danger"
+echo "##### Configure Danger"
 
-echo "Check Ruby version"
+echo "##### Check Ruby version"
 ruby --version
 gem --version
 bundler --version
 
-echo "Install bundle"
+echo "##### Install bundle"
 bundle config path vendor/cache
 bundle install --jobs 4 --retry 3 --gemfile=.ci/Gemfile
 
-echo "Execute Danger"
-
-#bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
+echo "##### Execute Danger"
+bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
