3d2d92c9044c4e07bc2a054cbee43e572f3fef8a
Roger Martins <roger.8b@gmail.com>
2020-05-31 13:06:44 -0300

    Fix: Remove Danger Workflow (#14)

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+echo "##### Install Ktlint"
+brew install ktlint
+
 echo "##### Execute Danger"
 bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
