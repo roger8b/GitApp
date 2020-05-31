2ef999621c9c865b88ea3753483b91a081e2a57b
Roger <roger.8b@gmail.com>
2020-05-30 22:05:51 -0300

    Test actions running script

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Actions running"
+set -euo pipefail
+
+echo "Configure Danger"
+
+echo "Check Ruby version"
+ruby --version
+gem --version
+bundler --version
+
+echo "Execute Danger"
+
+#bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
