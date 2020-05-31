580a78b65eacb30fed805b4083f764db8a62e8df
Roger <roger.8b@gmail.com>
2020-05-30 23:33:34 -0300

    Test actions running script

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 echo "##### Execute Danger"
-bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/statchic_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
+bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
