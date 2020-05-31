d360adf0b6677b85164e2d3fcdfa4c2a144389bb
Roger <roger.8b@gmail.com>
2020-05-31 17:06:18 -0300

    Danger Install Detekt

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -4,6 +4,7 @@ set -euo pipefail
 
 echo "##### Install Ktlint"
 # brew install ktlint
+tree
 
 echo "##### Execute Danger"
 bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=false
