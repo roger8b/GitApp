4d2b54f8b3ab14862e7b94b243f0fdd8819a88c1
Roger <roger.8b@gmail.com>
2020-05-31 01:05:15 -0300

    Fix Ktlint

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "##### Install Ktlint"
-curl --output /usr/local/bin/ktlint -sL https://github.com/pinterest/ktlint/releases/download/0.36.0/ktlint && chmod a+x /usr/local/bin/ktlint
+brew install ktlint
 
 echo "##### Execute Danger"
 bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
