48f12dc6603ec83c50e0fdd667ffbc6887dc8288
Roger Martins <roger.8b@gmail.com>
2020-05-31 22:42:19 -0300

    Added: Basic CI config (#19)

--- .ci/scripts/run.bash
+++ .ci/scripts/run.bash
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
+echo "##### Install Ktlint"
+curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.36.0/ktlint &&
+  chmod a+x ktlint &&
+  sudo mv ktlint /usr/local/bin/
+
 echo "##### Execute Danger"
 bundle exec --gemfile=.ci/Gemfile danger --dangerfile=".ci/danger/static_analysis.Dangerfile" --danger_id='static_analysis' --remove-previous-comments --fail-on-errors=true
